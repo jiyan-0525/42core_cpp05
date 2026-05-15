063e7f974b237fdd9db6697bfb8daa6cbf7af45a
Fiona-0525 <jiyanw825@gmail.com>
2026-05-15 22:26:17 +0200

    homeoff

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,9 +3,16 @@
 
 int main() {
     try {
-        Bureaucrat b1("Alice", 151);
+        Bureaucrat b1("Alice", 1);
         std::cout << b1 << std::endl;
         b1.incrementGrade();
+    } catch (const std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    try {
+        Bureaucrat b2("Bob", 150);
+        std::cout << b2 << std::endl;
+        b2.decrementGrade();
     } catch (const std::exception &e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
