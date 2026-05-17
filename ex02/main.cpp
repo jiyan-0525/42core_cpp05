11a93e53b66a4122095bf2074ccc77cad6bae524
Jiyan Wang <jiyawang@3-B-13.42heilbronn.de>
2026-05-17 17:25:59 +0200

    add ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,33 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "-----Test 1: Signing Forms-----" << std::endl;
+    try {
+        Bureaucrat john("John", 150);
+        Form taxForm("Tax-Form", 30,50);
+        std::cout << john << std::endl;
+        std::cout << taxForm << std::endl;
+
+        john.signForm(taxForm);
+        std::cout << taxForm << std::endl;
+    } catch (const std::exception &e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    std::cout << "\n-----Test 2: Form too low grade-----" << std::endl;
+    try {
+        Bureaucrat low("LowRank", 100);
+        Form restrictedForm("RestrictedForm", 50, 40);
+        std::cout << low << std::endl;
+        std::cout << restrictedForm << std::endl;
+
+        low.signForm(restrictedForm);
+        std::cout << restrictedForm << std::endl;
+    } catch (const std::exception &e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
