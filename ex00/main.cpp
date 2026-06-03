0cc097f9e90ed950e9083ca9f84541120f563362
Fiona8525 <jiyanwang@mail.de>
2026-06-03 08:27:43 +0000

    feat: updateex01
    Co-authored-by: Fiona8525 <jiyanwang@mail.de>

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 int main() {
-    Bureaucrat bob("Bob", 3);
-    Bureaucrat alice("Alice", 130);
+    Bureaucrat bob("Bob", 1);
+    Bureaucrat alice("Alice", 150);
     std::cout << bob << std::endl;
     std::cout << alice << std::endl;
     try {
